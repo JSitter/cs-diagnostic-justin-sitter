bf631c564ed2dac710d1c279e274effe2b1bdd08
Justin Sitter <suddenlysquirrels@yahoo.com>
2017-09-07 18:08:07 -0700

    Started solution 10

--- functions.py
+++ functions.py
@@ -10,4 +10,14 @@ def fibonacci_iterative(num = 10):
 		prev = number
 		number = number + prev	
 	
-	
+#solutions to problem 9
+#unfortunately I don't remember how to do this offhand :(
+#I believe one would have to import the os or sys modules for this one
+
+#Solution to problem 10
+def factorial_recursive(num):
+	if num > 1:
+		new_num = num - 1
+		factorial_recursive(new_num)
+	else:
+		
