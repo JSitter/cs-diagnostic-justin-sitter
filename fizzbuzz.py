9c9d8d231aa9f4e4c09cd86d55085e6b4244d415
Justin Sitter <suddenlysquirrels@yahoo.com>
2017-09-07 17:45:38 -0700

    completed fizzbuzz.py

---
+++ fizzbuzz.py
@@ -0,0 +1,9 @@
+# start = initial_starting_number
+# end = final_number
+# index = 0
+# numbers_between = end - start
+
+# check if numbers_between is positive or negative
+# if number_between > 0:
+#	if start + index is not divisible by 3
+#		print start + index 
