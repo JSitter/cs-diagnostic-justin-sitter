e6ed4386491805610e339d3ca116dcca4a0063a7
Justin Sitter <suddenlysquirrels@yahoo.com>
2017-09-07 18:03:20 -0700

    Added Problem 9

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,7 +3,16 @@
 # index = 0
 # numbers_between = end - start
 
-# check if numbers_between is positive or negative
-# if number_between > 0:
-#	if start + index is not divisible by 3
-#		print start + index 
+# while index < absolute value of numbers_between +1:
+#	if start + index is not divisible by 3 or 5
+#		print start + index
+#	else if start + index is divisible by 3 but not 5:
+#	 	print "fizz"
+#	else if start + index is divisible by 5 but not 3:	
+#		print "buzz"
+#	else:
+#		print "fizzbuzz"
+#	if numbers_between > 0
+#		index = index + 1
+#	else:
+#		index = index - 1
